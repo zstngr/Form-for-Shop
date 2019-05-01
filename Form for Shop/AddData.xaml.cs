69afab6b3914e2a8fe3596dd90227798c8c8e247
Артур Закиров <zstngr@gmail.com>
2019-05-01 23:55:30 +0300

    new features

---
+++ Form for Shop/AddData.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Form_for_Shop
+{
+    /// <summary>
+    /// Логика взаимодействия для AddData.xaml
+    /// </summary>
+    public partial class AddData : Window
+    {
+        public string MovieTitle;
+        public string Director;
+        public string MovieStartDate;
+        public int Price; 
+
+        public AddData()
+        {
+            InitializeComponent();
+        }
+        public void AddData_Load(object sender, EventArgs e)
+        {
+            MovieTitle = "MovieTitle";
+            Director = "Director";
+            MovieStartDate = "01:01:2000";
+            Price = 0;
+
+            TitleBox.Text = MovieTitle;
+            DirectorBox.Text = Director;
+            MovieStartDateBox.Text = MovieStartDate;
+            PriceBox.Text = Price.ToString();
+        }
+        public void Add_Click(object sender, EventArgs e)
+        {
+            MovieTitle = TitleBox.Text;
+            Director = DirectorBox.Text;
+            MovieStartDate = MovieStartDateBox.Text;
+            Price = Convert.ToInt32(PriceBox.Text);
+            DialogResult = true;
+        }
+        public void Cancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Form for Shop/MainWindow.xaml.cs
+++ Form for Shop/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SQLite;
+using System.IO;
+using System.Data;
 
 namespace Form_for_Shop
 {
@@ -20,9 +23,153 @@ namespace Form_for_Shop
     /// </summary>
     public partial class MainWindow : Window
     {
+        private String dbFileName;
+        private SQLiteConnection dbConnection;
+        private SQLiteCommand dbCommand;
+
         public MainWindow()
         {
             InitializeComponent();
+            dbFileName = "Movies.sqlite";
+            dbConnection = new SQLiteConnection();
+            dbCommand = new SQLiteCommand();
+
+            statusConnection.Text = "Disconnected";
+            
+        }
+
+        private void ButtonCreate_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(dbFileName))
+            {
+                SQLiteConnection.CreateFile(dbFileName);
+            }
+            else
+            {
+                MessageBox.Show("Database already exists");
+            }
+            try
+            {
+                dbConnection = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                dbConnection.Open();
+                dbCommand.Connection = dbConnection;
+
+                dbCommand.CommandText = "CREATE TABLE IF NOT EXISTS Catalog (id INTEGER PRIMARY KEY AUTOINCREMENT, Title TEXT, Director TEXT, MovieStartDate TEXT, Price INT)";
+                dbCommand.ExecuteNonQuery();
+                statusConnection.Text = "Connected";
+            }
+            catch (SQLiteException ex)
+            {
+                statusConnection.Text = "Disconnected";
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+        private void ButtonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dTable = new DataTable();
+            String sqlQuery;
+            try
+            {
+                dbConnection = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                dbConnection.Open();
+                dbCommand.Connection = dbConnection;
+                statusConnection.Text = "Connected";
+            }
+            catch (SQLiteException ex)
+            {
+                statusConnection.Text = "Disconnected";
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            try
+            {
+                sqlQuery = "SELECT * FROM Catalog";
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, dbConnection);
+                adapter.Fill(dTable);
+
+                dataGridMovies.ItemsSource = dTable.DefaultView;
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dTable = new DataTable();
+            String sqlQuery;
+            try
+            {
+                dbConnection = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                dbConnection.Open();
+                dbCommand.Connection = dbConnection;
+                statusConnection.Text = "Connected";
+            }
+            catch (SQLiteException ex)
+            {
+                statusConnection.Text = "Disconnected";
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            try
+            {
+                sqlQuery = "SELECT * FROM Catalog";
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, dbConnection);
+                adapter.Fill(dTable);
+
+                dataGridMovies.ItemsSource = dTable.DefaultView;
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void ButtonClearTable_Click(object sender, RoutedEventArgs e)
+        {
+            //DataTable dTable = new DataTable();
+            //dataGridMovies.ItemsSource = dTable.DefaultView;
+            //dataGridMovies.Items.Refresh();
+            try
+            {
+                dbConnection = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                dbConnection.Open();
+                dbCommand.Connection = dbConnection;
+
+                dbCommand.CommandText = "DELETE FROM Catalog WHERE id > 0";
+                dbCommand.ExecuteNonQuery();
+                dataGridMovies.Items.Refresh();
+            }
+            catch (SQLiteException ex)
+            {
+                statusConnection.Text = "Disconnected";
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (dbConnection.State != ConnectionState.Open)
+            {
+                MessageBox.Show("Open connection with database");
+                return;
+            }
+            AddData addData = new AddData();
+            if (addData.ShowDialog() == true)
+            {
+                try
+                {
+                    dbCommand.CommandText = $"INSERT INTO Catalog('Title', 'Director', 'MovieStartDate', 'Price') " +
+                        $"values ('{addData.MovieTitle}' , '{addData.Director}' , '{addData.MovieStartDate}', '{addData.Price}')";
+                    dbCommand.ExecuteNonQuery();
+                }
+                catch (SQLiteException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
         }
     }
+
 }
